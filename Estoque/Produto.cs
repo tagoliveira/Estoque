5003b9e846ca8728cc84a008c7a415b45e581fab
tagoliveira <tagoliveira.dev@gmail.com>
2021-10-18 18:36:36 -0300

    Add project files.

---
+++ Estoque/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    //public = sem restricoes
+    //private = somente podem ser acessados na classe
+    //protected = semelhante ao private e podem ser acessados de outras classes
+    //internal e protected internal
+    public class Produto
+    {
+        private string nome_produto;
+        private double preco;
+
+        public Produto() //metodo construtor
+        {
+            Console.WriteLine("Iniciou a classe Produto atravéz de seu método Construtor");
+        }
+
+        /*public void setNome (string nome_produto)
+        {
+            this.nome_produto = nome_produto;
+        }
+
+        public string getNome()
+        {
+            return nome_produto;
+        }*/
+
+        public string Nome_produto
+        {
+            get { return nome_produto; }
+            set { nome_produto = value; }
+        }
+
+        /*public void setPreco(double preco)
+        {
+            this.preco = preco;
+        }
+
+        public double getPreco()
+        {
+            return preco;
+        }*/
+        public double Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+    }
+}
---
+++ Estoque/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando a classe produto
+            Produto objeto_instanciado_produto = new Produto();
+            //objeto_instanciado_produto.nome_produto = "Melancia";
+            //objeto_instanciado_produto.preco = 10;
+            //Console.WriteLine("Produto = " + objeto_instanciado_produto.nome_produto);
+            //Console.WriteLine("Preço = " + objeto_instanciado_produto.preco);
+            /*objeto_instanciado_produto.setNome("Melancia");
+            Console.WriteLine("Produto = " + objeto_instanciado_produto.getNome());
+            objeto_instanciado_produto.setPreco(10);
+            Console.WriteLine("Preço = " + objeto_instanciado_produto.getPreco());
+            objeto_instanciado_produto.setNome("Melão");
+            Console.WriteLine("Produto = " + objeto_instanciado_produto.getNome());
+            objeto_instanciado_produto.setPreco(5);
+            Console.WriteLine("Preço = " + objeto_instanciado_produto.getPreco());
+            */
+            //objeto_instanciado_produto.setPreco(10);
+            //Console.WriteLine("Preço = " + objeto_instanciado_produto.getPreco());
+            objeto_instanciado_produto.Nome_produto = "Melancia";
+            Console.WriteLine("Produto = " + objeto_instanciado_produto.Nome_produto);
+            objeto_instanciado_produto.Preco = 10;
+            Console.WriteLine("Preço = " + objeto_instanciado_produto.Preco);
+
+            Console.ReadKey();
+        }
+    }
+}
